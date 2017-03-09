ee9d36c016683154a4dc39b840a9925a14ff2743
balamurugan1492 <balamurugan@pointelsolutions.com>
2017-03-09 21:24:45 +0530

    change method name

--- MyOrg/testBala/Interface1.cs
+++ MyOrg/testBala/Interface1.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MyOrg.testBala
+﻿namespace MyOrg.testBala
 {
-    interface Interface1
+    internal interface Interface1
     {
-        void method();
+        void method1();
     }
 }
