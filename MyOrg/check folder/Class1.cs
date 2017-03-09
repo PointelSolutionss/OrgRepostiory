3b3ea6ca2ee061d67a6fd0d3da8d9a06ccb194ff
ranjithsundaram470 <ranjith.meenatchi@pointelsolutions.com>
2017-03-09 20:48:24 +0530

    added class file

---
+++ MyOrg/check folder/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyOrg.check_folder
+{
+    class Class1
+    {
+    }
+}
